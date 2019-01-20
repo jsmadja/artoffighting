fa120fee20e46baa8c30ad118ef481007031ed2c
Julien Smadja <julien.smadja@gmail.com>
2019-01-20 18:19:53 +0100

    Move

--- compilable/3_rebuild_rom.bat
+++ compilable/3_rebuild_rom.bat
@@ -2,3 +2,4 @@ cd AOF\bin
 call makerom
 copy 044-p1.p1 C:\Users\necta\Documents\Final_Burn_Alpha_0.2.97.43_Fr\roms /Y
 cd ..\..
+powershell -Command "& { Compress-Archive -Update -Path C:\Users\necta\Documents\Final_Burn_Alpha_0.2.97.43_Fr\roms\044-p1.p1 Final_Burn_Alpha_0.2.97.43_Fr\roms\aof.zip}"
--- compilable/AOF/bin/makerom.bat
+++ compilable/AOF/bin/makerom.bat
@@ -1,13 +1,13 @@
 del 044-p1.p1
 del rom.bin
-..\..\rom_add\Release\rom_add rom.bin 0x80000 GAME.OUT 0x0 
-..\..\rom_add\Release\rom_add rom.bin 0x80000 SYS.OUT 0xa00 0x11600
-..\..\rom_add\Release\rom_add rom.bin 0x80000 SYS_DATA.BIN 0x4f40
-..\..\rom_add\Release\rom_add rom.bin 0x80000 HERO.OUT 0x12000 0x7000
-..\..\rom_add\Release\rom_add rom.bin 0x80000 HK3.OUT 0x19000 0xD000
-..\..\rom_add\Release\rom_add rom.bin 0x80000 CHARA.OUT 0x26000 0x17800
-..\..\rom_add\Release\rom_add rom.bin 0x80000 DEMO_DATA.bin 0x3AA90
-..\..\rom_add\Release\rom_add rom.bin 0x80000 LANG.OUT 0x3d800
-..\..\rom_add\Release\rom_add rom.bin 0x80000 OTHER_DATA.bin 0x42b00
+..\..\rom_add\Release\rom_add rom.bin 0x100000 GAME.OUT 0x0
+..\..\rom_add\Release\rom_add rom.bin 0x100000 SYS.OUT 0xa00 0x11600
+..\..\rom_add\Release\rom_add rom.bin 0x100000 SYS_DATA.BIN 0x4f40
+..\..\rom_add\Release\rom_add rom.bin 0x100000 HERO.OUT 0x12000 0x7000
+..\..\rom_add\Release\rom_add rom.bin 0x100000 HK3.OUT 0x19000 0xD000
+..\..\rom_add\Release\rom_add rom.bin 0x100000 CHARA.OUT 0x26000 0x17800
+..\..\rom_add\Release\rom_add rom.bin 0x100000 DEMO_DATA.bin 0x3AA90
+..\..\rom_add\Release\rom_add rom.bin 0x100000 LANG.OUT 0x80000
+..\..\rom_add\Release\rom_add rom.bin 0x100000 OTHER_DATA.bin 0x42b00
 byteswap.exe rom.bin
 ren rom.bin 044-p1.p1
